7c8d90a98b866261742c0014d756e46432b9c0db
Sonnycode <sonnywen26@gmail.com>
2022-09-19 16:03:19 +0700

    Membuat DOM dalam miniproject

---
+++ Sonny_DOM.js
@@ -0,0 +1,6 @@
+let div = document.getElementsByClassName('DOM')[0];
+div.innerHTML = 'Anda belum melakukan Login';
+div.style.color = 'red';
+div.style.textAlign = 'center';
+document.body.appendChild(div);
+
